4c79b1b4add7c23c618e1a9c2332b3f2ee4545ab
Yadunand <yadunut@gmail.com>
2017-10-14 02:51:52 +0800

    Added stuff

--- app/controllers/concerns/fact_checker.rb
+++ app/controllers/concerns/fact_checker.rb
@@ -1,7 +1,14 @@
 module FactChecker
   def self.perform(article)
-    scores =
-      Checker.all.map do |checker|
-      end
+    old_rating =
+        Checker.all.map do |checker|
+          checker.score
+        end
+
+    new_rating =
+        Checker.all.map do |checker|
+
+        end
+
   end
 end
