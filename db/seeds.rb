d9555909d1f484f29518284c5a5ff054cc37f02a
Yadunand <yadunut@gmail.com>
2017-10-14 14:48:56 +0800

    Made seeds more dope

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Checker.destroy_all
-Checker.create(score: 1, fake_id: 1)
-Checker.create(score: 1, fake_id: 2)
-Checker.create(score: 1, fake_id: 3)
-Checker.create(score: 1, fake_id: 4)
-Checker.create(score: 1, fake_id: 5)
+Checker.create(score: 1, fake_id: 1, name: 'CheckerNewsAsia', description: 'Resilient, strong, fact checker')
+Checker.create(score: 1, fake_id: 2, name: 'CheckFact', description: 'Checker of only facts')
+Checker.create(score: 1, fake_id: 3, name: 'NowFact', description: 'Realtime fact checking at your fingertips')
+Checker.create(score: 1, fake_id: 4, name: 'fact.ly', description: 'A new way to fact check')
+Checker.create(score: 1, fake_id: 5, name: 'Factr', description: 'Increase your fact checking tenfold')
 Checker.all.each do |checker|
   puts checker.fake_id
 end
